4a4812131966deb5a809acd64520c3b591dc28ae
Gunasekar Jabbala <gunasekar.ai.dev@gmail.com>
2023-08-30 12:10:23 +0530

    Update solver.py

--- projects/sudoku-solver-ai-agent/exercise/solver.py
+++ projects/sudoku-solver-ai-agent/exercise/solver.py
@@ -1,14 +1,63 @@
-def solve(puzzle: list(list(int))) -> bool:
+def is_valid(puzzle: list, row: int, col: int, num: int) -> bool:
+    """ Function checks for the valid entry
+
+    Args:
+        puzzle (list): puzzle 2Dboard
+        row (int): row index number
+        col (int): column index number
+        num (int): valid number to check wether to update or not
+
+    Returns:
+        bool: if it is valid number return True, else False
+    """
+    for i in range(9):
+        if puzzle[row][i] == num or puzzle[i][col] == num:
+            return False
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                if puzzle[start_row + i][start_col + j] == num:
+                    return False
+        return True
+
+
+def solve(puzzle) -> bool:
     """ The solve function defined to take the 2D and 9*9 sudoku to solve and 
     fill all zeros with appropriate numbers.
 
     Args:
-        puzzle (list): _description_
+        puzzle (list): puzzle
 
     Returns:
-        bool: _description_
+        bool: solved or no solution
     """
-    return False
+    
+    for row in range(9):
+        for col in range(9):
+            if puzzle[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(puzzle, row, col, num):
+                        puzzle[row][col] = num
+                        if solve(puzzle):
+                            return True
+                        puzzle[row][col] = 0
+                return False
+    return True
+
+def print_board(puzzle) -> None:
+    """ Display puzzle board
+
+    Args:
+        puzzle (list): Puzzle board
+    """
+    for i in range(9):
+        for j in range(9):
+            print(puzzle[i][j], end=" ")
+            if j == 2 or j == 5:
+                print("|", end=" ")
+        print()
+        if i == 2 or i == 5:
+            print("-" * 21)
 
 
 if __name__ == "__main__":
@@ -25,7 +74,10 @@ if __name__ == "__main__":
     ]
     
     
+    print(type(puzzle))
+    
     if solve(puzzle):
         print("Sudoku puzzle solved:")
+        print_board(puzzle)
     else:
         print("No solution exists for the puzzle.")
