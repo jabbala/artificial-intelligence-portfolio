3fd8be2908929c0e22ea02b9a8f704f41925df55
Gunasekar Jabbala <gunasekar.ai.dev@gmail.com>
2023-08-30 16:05:41 +0530

    Update solver.py

--- projects/sudoku-solver-ai-agent/exercise/solver.py
+++ projects/sudoku-solver-ai-agent/exercise/solver.py
@@ -1,4 +1,16 @@
-def is_valid(puzzle: list, row: int, col: int, num: int) -> bool:
+"""Solve Sudoku Problem"""
+PUZZLE_OUTER: list = [
+        [0, 9, 8, 6, 4, 1, 0, 3, 7],
+        [0, 0, 7, 8, 0, 0, 9, 4, 6],
+        [0, 3, 6, 0, 0, 9, 8, 2, 0],
+        [8, 5, 9, 2, 3, 6, 1, 7, 0],
+        [0, 0, 0, 0, 9, 8, 3, 5, 2],
+        [3, 0, 0, 1, 0, 7, 6, 9, 8],
+        [0, 6, 0, 0, 1, 0, 4, 0, 5],
+        [9, 0, 5, 7, 6, 4, 0, 1, 0],
+        [0, 0, 4, 3, 8, 5, 0, 0, 9],
+    ]
+def is_valid(row: int, col: int, num: int) -> bool:
     """Function checks for the valid entry
 
     Args:
@@ -11,17 +23,17 @@ def is_valid(puzzle: list, row: int, col: int, num: int) -> bool:
         bool: if it is valid number return True, else False
     """
     for i in range(9):
-        if puzzle[row][i] == num or puzzle[i][col] == num:
+        if PUZZLE_OUTER[row][i] == num or PUZZLE_OUTER[i][col] == num:
             return False
         start_row, start_col = 3 * (row // 3), 3 * (col // 3)
         for i in range(3):
             for j in range(3):
-                if puzzle[start_row + i][start_col + j] == num:
+                if PUZZLE_OUTER[start_row + i][start_col + j] == num:
                     return False
     return True
 
 
-def solve(puzzle) -> bool:
+def solve() -> bool:
     """To solve and fill all zeros with appropriate numbers.
 
     Args:
@@ -32,48 +44,39 @@ def solve(puzzle) -> bool:
     """
     for row in range(9):
         for col in range(9):
-            if puzzle[row][col] == 0:
+            if PUZZLE_OUTER[row][col] == 0:
                 for num in range(1, 10):
-                    if is_valid(puzzle, row, col, num):
-                        puzzle[row][col] = num
-                        if solve(puzzle):
+                    if is_valid(row, col, num):
+                        PUZZLE_OUTER[row][col] = num
+                        if solve():
                             return True
-                        puzzle[row][col] = 0
+                        PUZZLE_OUTER[row][col] = 0
                 return False
     return True
 
 
-def print_board(puzzle) -> None:
+def print_board() -> None:
     """Display puzzle board
 
     Args:
         puzzle (list): Puzzle board
     """
+    print("-" * 25)
     for i in range(9):
+        print("|", end=" ")
         for j in range(9):
-            print(puzzle[i][j], end=" ")
-            if j == 2 or j == 5:
+            print(PUZZLE_OUTER[i][j], end=" ")
+            if j == 2 or j == 5 or j == 8:
                 print("|", end=" ")
         print()
         if i == 2 or i == 5:
-            print("-" * 21)
-
+            print("-" * 25)
+    print("-" * 25)
 
 if __name__ == "__main__":
-    puzzle = [
-        [0, 9, 8, 6, 4, 1, 0, 3, 7],
-        [0, 0, 7, 8, 0, 0, 9, 4, 6],
-        [0, 3, 6, 0, 0, 9, 8, 2, 0],
-        [8, 5, 9, 2, 3, 6, 1, 7, 0],
-        [0, 0, 0, 0, 9, 8, 3, 5, 2],
-        [3, 0, 0, 1, 0, 7, 6, 9, 8],
-        [0, 6, 0, 0, 1, 0, 4, 0, 5],
-        [9, 0, 5, 7, 6, 4, 0, 1, 0],
-        [0, 0, 4, 3, 8, 5, 0, 0, 9],
-    ]
 
-    if solve(puzzle):
+    if solve():
         print("Sudoku puzzle solved:")
-        print_board(puzzle)
+        print_board()
     else:
         print("No solution exists for the puzzle.")
